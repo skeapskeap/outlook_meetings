bdf3858d94d73d0d64bbf675bd37b88a6fcf9154
FIRST_NAME LAST_NAME <skeap@cn.ru>
2020-07-30 15:53:54 +0700

    check last hour emails

--- parse_mail.py
+++ parse_mail.py
@@ -5,13 +5,11 @@ https://devblogs.microsoft.com/premier-developer/outlook-email-automation-with-p
 https://community.spiceworks.com/how_to/150253-send-mail-from-powershell-using-outlook
 """
 
-from datetime import datetime as dt
+from datetime import datetime as dt, timedelta
 import re
 import win32com.client
 
-
 outlook = win32com.client.Dispatch('Outlook.Application').GetNamespace("MAPI")  # Это магия чтобы обращаться к COM-объектам Outlook.Application
-
 """
     Эти штуки итерируются о_О
     for item in email_boxes:
@@ -21,11 +19,7 @@ email_boxes = outlook.Folders  # Все ящики, добавленные в Ou
 my_box = email_boxes.Item(6)        # это мой личный ящик
 my_inbox = my_box.Folders.Item(4)   # это папка Входящие в моём ящике
 target_folder = my_inbox.Folders.Item('Notifications')  # Можно даже по имени папку указывать
-
-emails = target_folder.Items
-subj = emails(1).Subject
-time = emails(1).SentOn
-body = emails(1).Body
+all_emails = target_folder.Items
 
 
 def parse_notification(notification: str) -> dict:
@@ -41,10 +35,21 @@ def parse_notification(notification: str) -> dict:
     meeting_data = {'node':         node_address,
                     'time_start':   time_start,
                     'url':          url}
-
     return meeting_data
 
 
+def proper_dt(com_dt):
+    date_string = dt.strftime(com_dt, '%d.%m.%y %H:%M:%S')       # Преобразование datetime COM-объекта в строку
+    standard_dt = dt.strptime(date_string, '%d.%m.%y %H:%M:%S')  # Преобразование строки в стандартный datetime
+    return standard_dt
+
+
+def last_hour_emails(emails) -> list:
+    hour_ago = dt.now()-timedelta(hours=1)
+    notifications = [item.Body for item in emails if proper_dt(item.SentOn) > hour_ago]
+    return notifications
+
+
 if __name__ == '__main__':
-    meeting_data = parse_notification(body)
-    print(meeting_data)
+    for item in last_hour_emails(all_emails):
+        print(parse_notification(item))
