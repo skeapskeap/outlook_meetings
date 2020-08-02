e3d2e3e15191f8ae5f22bca454526b3e8eb880c1
FIRST_NAME LAST_NAME <skeap@cn.ru>
2020-08-02 15:09:52 +0700

    expose settings.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+DURATION                    = 60  # 60 minutes = 1 hour
+MEETING_RECIPIENT           = 'user@example.com'
+OUTLOOK_APPOINTMENT_ITEM    = 1
+OUTLOOK_MEETING             = 1
+OUTLOOK_ORGANIZER           = 0
+OUTLOOK_OPTIONAL_ATTENDEE   = 2
+OUTLOOK_FORMAT              = '%d/%m/%Y %H:%M'
+REMIND                      = 15  # 15 minutes
+TIME_SPAN                   = 24  # 24 hours
